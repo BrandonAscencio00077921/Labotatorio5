cdd06a8a0d360eb41f2d503de9dba4c8596e8957
Elias8420 <eliaszelaya747@gmail.com>
2026-06-20 04:10:00 -0600

    add: entity user

---
+++ src/main/java/org/example/buhosapp/domain/entities/User.java
@@ -0,0 +1,33 @@
+package org.example.buhosapp.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String username;
+
+    @Column(name = "card")
+    private String card;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
