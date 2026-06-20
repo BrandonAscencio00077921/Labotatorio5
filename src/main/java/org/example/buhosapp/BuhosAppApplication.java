8afeadbc62a79d496fbedd3096e81d5f16905f07
Elias8420 <eliaszelaya747@gmail.com>
2026-06-19 19:33:19 -0600

    add: Base project

---
+++ src/main/java/org/example/buhosapp/BuhosAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.buhosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuhosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuhosAppApplication.class, args);
+    }
+
+}
